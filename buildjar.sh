780e13595dd774e78d19e0a9ba660902f60f7751
Niclas Kjäll-Ohlsson <niclasko@gmail.com>
2021-10-06 13:31:10 +0200

    Change

--- buildjar.sh
+++ buildjar.sh
@@ -10,6 +10,3 @@ for i in `ls ./libs` ; do unzip -o ./libs/$i -d ./jarcontents ; done
 cd ./jarcontents
 
 jar cvfe ../build/GA-jdbc.jar org.sebson.jdbc.GA.GADriver ./*
-
-# Run it:
-#   java -jar ./build/GA-jdbc.jar KEY_FILE_LOCATION=dev-mg-950381754252.json "select ga:city, ga:pageTitle, ga:dateHourMinute, ga:country, ga:sessions from 'your_google_view_id' where date between 10DaysAgo and today"
